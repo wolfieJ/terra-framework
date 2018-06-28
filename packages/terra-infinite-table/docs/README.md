84be13f5c699bcebb4a416735c53851e46cf979e
Kasper, Dave <dave.m.kasper@gmail.com>
2018-06-27 20:29:55 -0500

    Initial file structure creation.

---
+++ packages/terra-infinite-table/docs/README.md
@@ -0,0 +1,15 @@
+# Terra Infinite Table
+
+The infinite table component provides virtual dom management and data request callbacks to manage large data sets within a table.
+InfiniteTable is a controlled HOC built upon the terra-table component in order to allow for table style and selection patterns.
+
+## Getting Started
+
+- Install with [npmjs](https://www.npmjs.com):
+  - `npm install terra-infinite-table`
+
+
+## Component Features
+* [Cross-Browser Support](https://github.com/cerner/terra-core/wiki/Component-Features#cross-browser-support)
+* [Responsive Support](https://github.com/cerner/terra-core/wiki/Component-Features#responsive-support)
+* [Mobile Support](https://github.com/cerner/terra-core/wiki/Component-Features#mobile-support)
---
+++ packages/terra-infinite-table/src/terra-dev-site/doc/infinite-table/InfiniteTable.1.doc.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-duplicates */
+import React from 'react';
+import DocTemplate from 'terra-doc-template';
+import { name } from '../../../../package.json';
+import ReadMe from '../../../../docs/README.md';
+
+// Component Source
+import InfiniteTableSrc from '!raw-loader!../../../../src/InfiniteTable.jsx';
+import InfiniteTableExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/InfiniteTableExample.jsx';
+
+// Example Files
+import InfiniteTableExample from '../example/InfiniteTableExample';
+
+const DocPage = () => (
+  <DocTemplate
+    packageName={name}
+    readme={ReadMe}
+    srcPath={`https://github.com/cerner/terra-framework/tree/master/packages/${name}`}
+    examples={[
+      {
+        title: 'Example Infinite List',
+        example: <InfiniteTableExample />,
+        source: InfiniteTableExampleSrc,
+      },
+    ]}
+    propsTables={[
+      {
+        componentSrc: InfiniteTableSrc,
+      },
+    ]}
+  />
+);
+
+export default DocPage;
