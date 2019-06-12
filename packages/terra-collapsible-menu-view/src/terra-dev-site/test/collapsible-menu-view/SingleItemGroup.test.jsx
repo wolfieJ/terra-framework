32301fad3fe6bf8b278619c216ed1ca7261aecee
Pranav Agarwal <Pranav.Agarwal@cerner.com>
2019-06-12 19:22:51 +0530

    updated source code.

--- packages/terra-collapsible-menu-view/src/terra-dev-site/test/collapsible-menu-view/SingleItemGroup.test.jsx
+++ packages/terra-collapsible-menu-view/src/terra-dev-site/test/collapsible-menu-view/SingleItemGroup.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CollapsibleMenuView from '../../../CollapsibleMenuView';
-// const locales = ['en', 'en-AU', 'en-US', 'en-GB', 'es', 'es-US', 'es-ES', 'de', 'fi-FI', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE'];
-const locales = ['en', 'en-AU', 'en-US', 'en-GB', 'es', 'es-US', 'es-ES', 'de', 'fi-FI', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE', 'es-ES', 'de', 'fi-FI', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE'];
+const locales = ['en', 'en-AU', 'en-US', 'en-GB', 'es', 'es-US', 'es-ES', 'de', 'fi-FI', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE'];
 const CollapsibleMenuViewSingleItem = () => {
   const items = [];
   items.push(
@@ -17,28 +16,9 @@ const CollapsibleMenuViewSingleItem = () => {
     </CollapsibleMenuView.ItemGroup>,
   );
   return (
-  // Previous Implementation try with 67% till 100% browser size
-  /* <CollapsibleMenuView key="menu">
-      {items}
-    </CollapsibleMenuView> */
-
-  /* Both of these methods works for both one large single item group and more than one item group
-     What I have found with the previous implementation is that with only calling items in the collapse menu
-    it gives different results for different browser window size */
-
     <CollapsibleMenuView key="menu">
-      {items.map(item => (
-        <div>
-          {item}
-        </div>
-      ))}
+      {items}
     </CollapsibleMenuView>
-    /* <CollapsibleMenuView key="menu">
-      {items.length > 1 ? items.map(item => (
-          item
-        )) : items[0]
-      }
-    </CollapsibleMenuView > */
   );
 };
 
